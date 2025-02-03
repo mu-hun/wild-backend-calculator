a6e2d3bdd32e23ccda478ceaa966348a8f9b6f0b
Muhun Kim <iam@muhun.kim>
2025-02-03 16:34:52 +0900

    Initial implement HTTP Server

--- app/src/main/java/com/example/demo/App.java
+++ app/src/main/java/com/example/demo/App.java
@@ -1,7 +1,40 @@
 package com.example.demo;
 
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
+    public String getGreeting() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws IOException {
+        new App().run();
+    }
+
+    public void run() throws IOException {
+        InetSocketAddress address = new InetSocketAddress("localhost", 8080);
+        HttpServer httpServer = HttpServer.create(address, 0);
+
+        httpServer.createContext("/", exchange -> {
+            String method = exchange.getRequestMethod();
+            String path = exchange.getRequestURI().getPath();
+
+            System.out.println(method + " " + path);
+
+            byte[] greeting = getGreeting().getBytes();
+
+            exchange.sendResponseHeaders(200, greeting.length);
+
+            try (OutputStream os = exchange.getResponseBody()) {
+                os.write(greeting);
+            }
+        });
+        httpServer.start();
+
+        System.out.println("Listening on " + address);
     }
 }
