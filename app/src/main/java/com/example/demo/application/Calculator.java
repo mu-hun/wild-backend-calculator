54f6fdb34b057379b0c03bfd92e6ac72ff4b6844
Muhun Kim <iam@muhun.kim>
2025-02-08 01:31:46 +0900

    Improve the safety of calculate processing
    1. There is no exception when dividing by zero
    2. It is not appropriate to return zero when entering the wrong operator
    3. There is no null check for operator parameters

--- app/src/main/java/com/example/demo/application/Calculator.java
+++ app/src/main/java/com/example/demo/application/Calculator.java
@@ -10,16 +10,25 @@ public class Calculator {
     private final CalculationRepository calculationRepository = CalculationRepository.getInstance();
 
     public Calculation calculate(int a, int b, String operator) {
+        if (operator == null) {
+            throw new IllegalArgumentException("Operator is required");
+        }
         int result = switch (operator) {
             case "+" -> a + b;
             case "-" -> a - b;
             case "*" -> a * b;
-            case "/" -> a / b;
-            default -> 0;
+            case "/" -> {
+                if (b == 0) {
+                    throw new IllegalArgumentException("Division by zero");
+                }
+                yield a / b;
+            }
+            default ->
+                    throw new IllegalArgumentException("Invalid operator" + operator);
         };
 
         Calculation calculation = new Calculation(operator, a, b, result);
-        
+
         calculationRepository.add(calculation);
 
         return calculation;
