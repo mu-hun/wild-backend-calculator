25b24f08b7e2e2a526d04e0cf2050d34c480cd80
Muhun Kim <iam@muhun.kim>
2025-02-08 01:03:22 +0900

    Apply review suggestion about DTO records
    https://github.com/dal-lab/wild-backend-calculator/pull/13#discussion_r1946730753

--- app/src/main/java/com/example/demo/dto/CalculationListResponseDto.java
+++ app/src/main/java/com/example/demo/dto/CalculationListResponseDto.java
@@ -3,14 +3,11 @@ package com.example.demo.dto;
 import com.example.demo.infrastructure.Calculation;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
-public class CalculationListResponseDto implements Serializable {
-    private final List<CalculationResponseDto> calculations;
-
-    public CalculationListResponseDto(List<CalculationResponseDto> calculations) {
-        this.calculations = calculations;
-    }
+public record CalculationListResponseDto(
+        List<CalculationResponseDto> calculations) implements Serializable {
 
     public static CalculationListResponseDto of(List<Calculation> calculations) {
         return new CalculationListResponseDto(calculations.stream()
@@ -25,7 +22,8 @@ public class CalculationListResponseDto implements Serializable {
                 .toList());
     }
 
-    public List<CalculationResponseDto> getCalculations() {
-        return calculations;
+    @Override
+    public List<CalculationResponseDto> calculations() {
+        return new ArrayList<>(calculations);
     }
 }
